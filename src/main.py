ffa5478f0fa0f4532f55fcf22fbf71d670cb40fd
steven <steven@nztech.ca>
2024-11-30 11:37:25 -0800

    checks coverage and conv latlng to country

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import requests
+import random
+from geopy.geocoders import Nominatim
 from PIL import Image
 from io import BytesIO
 
@@ -15,16 +17,56 @@ def main():
         print(f"An error occurred: {e}")
         return None
 
-    lat, lng = 48.858844, 2.294351
+    # lat, lng = 48.858844, 2.294351
+    # lat, lng = 48.0, 2.0
+    lat, lng = 33.976621, -118.218250
+    valid_coverage, data = check_street_view_coverage(lat, lng, api_key)
+    if not valid_coverage:
+        return None
+    pano_id = data['pano_id']
+    print(pano_id)
+    print(data)
+    geolocator = Nominatim(user_agent="my-app")
+    location = geolocator.reverse(f"{lat},{lng}")
+
+    print(location.address)
+    print(location.address.split(", ")[-1])
+
+    lat = data['location']['lat']
+    lng = data['location']['lng']
+
     tiles = []
-    for heading in range(0, 360, 90):  # Fetch 3 tiles (0°, 120°, 240°)
+    for heading in range(0, 360, 90):
         row_tiles = []
         for pitch in [0]:  # Adjust pitch for 3 rows
             tile = fetch_streetview_tile(api_key, lat, lng, heading, pitch)
             row_tiles.append(tile)
         tiles.append(row_tiles)
     stitched_image = stitch_tiles(tiles)
-    stitched_image.save("stitched_streetview_3.jpg")
+    stitched_image.save(f"data/{lat}_{lng}_.jpg")
+
+
+def check_street_view_coverage(lat, lng, api_key):
+    """
+    Check if Google Street View imagery exists for the given latitude and longitude.
+    :param lat: Latitude.
+    :param lng: Longitude.
+    :return: True if imagery exists, False otherwise.
+    """
+    metadata_url = "https://maps.googleapis.com/maps/api/streetview/metadata"
+    params = {
+        "location": f"{lat},{lng}",
+        'radius': 10000,  # search radius in meters
+        "key": api_key,
+    }
+    response = requests.get(metadata_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200 and data["status"] == "OK":
+        return True, data
+    else:
+        print(f"No Street View imagery for {lat}, {lng}: {data.get('status', 'Unknown error')}")
+        return False, data
 
 
 def stitch_tiles(tiles):
